048428d2aa0bd36f477a837b1ec44269aca2f65f
tlieu19 <lieutimothy19@gmail.com>
2019-09-27 17:36:16 -0700

    Creating a LockPick Minigame

---
+++ Assets/TimothyLWireLockPicker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimothyLWireLockPicker : MonoBehaviour
+{
+    public Vector2 liftUp;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Does this work?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        while (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().velocity = liftUp;
+        }
+
+        
+
+
+    }
+
+    //Need to record WireLockPicker hitting Top or Bottom Border
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.relativeVelocity.magnitude > 2)
+        {
+            print("Whoa! You hit something!");
+        }
+    }
+}
