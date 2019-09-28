dd5e9a5b0821a7a2c786f73aa1774478cae73b14
tlieu19 <tlieu19@users.noreply.github.com>
2019-09-27 22:58:11 -0700

    Updates to Wire class

--- Assets/TimothyLWire.cs
+++ Assets/TimothyLWire.cs
@@ -17,13 +17,28 @@ public class TimothyLWire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        while (Input.GetKeyDown(KeyCode.Space))
+        //This is how they're going to move the lockpicking wire
+        if (Input.GetKey(KeyCode.Z))
         {
             GetComponent<Rigidbody2D>().velocity = liftUp;
         }
 
-        
+        //Creating a case where if it touches the top border, lock breaks and user has to start over
+        if (GetComponent<Rigidbody2D>().position.y > 2.69)
+        {
+
+        }
+
+        //They press this when they want to "turn" the wire and "pop" the lock open
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (GetComponent<Rigidbody2D>().position.y > 0)
+            {
+                print("Wow you made it work");
+            }
+            print (GetComponent<Rigidbody2D>().position);
+            
+        }
 
 
     }
