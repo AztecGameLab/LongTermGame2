7ab6aa96dcca500f67f1f918c8b7e98bb8bcabd1
Fallium <archer.hovey@gmail.com>
2019-09-29 16:22:12 -0700

    added targets to shoot

---
+++ Assets/AHTarget.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AHTarget : MonoBehaviour
+{
+    public int value;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "ArcherH Bullet(Clone)")
+        {
+            if(GameObject.Find("Score").GetComponent<Text>() == null)
+            {
+                print("finding score text didnt work");
+            }
+            Text score = GameObject.Find("Score").GetComponent<Text>();
+            if(score.GetComponent<AHScore>() == null)
+            {
+                print("finding script didnt work");
+            }
+            score.GetComponent<AHScore>().AddToScore(value);
+            Destroy(gameObject);
+        }
+    }
+}
