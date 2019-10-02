90c3273c4aa8427b221cc610aa12338df46663f5
Kainkun <kainkun100@gmail.com>
2019-10-01 22:03:21 -0700

    management of some files added pong360 namespace

---
+++ Assets/Scripts/Pong360/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace pong360
+{
+    public class Spawner : MonoBehaviour
+    {
+
+        void Start()
+        {
+            print("hello");
+        }
+
+
+        void Update()
+        {
+
+        }
+    }
+
+}
