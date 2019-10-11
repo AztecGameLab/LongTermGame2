172efecbb718aad5cd7cafe4ab709a178825dc02
Jacob Barger <jacob.barger@hotmail.com>
2019-10-11 14:05:13 -0700

    initial comit for run game

---
+++ Assets/Scripts/RunAwayThief/RunEnemyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RunAwayThief
+{
+    public class RunEnemyController : MonoBehaviour
+    {
+        public Vector2 moveForward;
+        public float increaseSpeed;
+        public BoxCollider2D playerBox;
+        void Start()
+        {
+            playerBox = GetComponent<BoxCollider2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+        public void EnemyRun()
+        {
+            this.GetComponent<Rigidbody2D>().velocity = moveForward;
+            this.GetComponent<Rigidbody2D>().AddForce(moveForward * increaseSpeed);
+        }
+    }
+}
