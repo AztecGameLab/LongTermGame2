0cffb3997544387c2c2e49dd64669a8521bd1756
Fallium <archer.hovey@gmail.com>
2019-10-19 17:10:50 -0700

    made functioning targets

---
+++ Assets/Scripts/Sniper/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject baddie;
+    public int numBadGuys;
+    public GameObject goodie;
+    public int numGoodGuys;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float xLower = -8f;
+        float xUpper = 8f;
+        float yLower = -3;
+        float yUpper = 3;
+        for (int i = 0; i <numBadGuys; i++)
+        {
+            Instantiate(baddie, new Vector2(Random.Range(xLower, xUpper), Random.Range(yLower, yUpper)), Quaternion.identity);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Sniper/baddie.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Sniper
+{
+    public class baddie : MonoBehaviour
+    {
+        public float speed;
+        Rigidbody2D rb;
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb = gameObject.GetComponent<Rigidbody2D>();
+            rb.velocity = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized * speed;
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+        private void OnMouseDown()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
