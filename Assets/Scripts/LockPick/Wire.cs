ca263688df9107b5e40f5964016d4bbdb2127ae5
tlieu19 <lieutimothy19@gmail.com>
2019-11-01 14:20:41 -0700

    Finishing LockPick

--- Assets/Scripts/LockPick/Wire.cs
+++ Assets/Scripts/LockPick/Wire.cs
@@ -1,24 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace LockPick{
     public class Wire : MonoBehaviour
     {
-        public Vector2 liftUp;
-        public Vector2 startPosition;
+        private Vector2 liftUp = new Vector2(0, 3f);
+        private Vector2 startPosition;
+        private Vector2 successPosition;
 
         public GameObject secondWire;
 
-        public static int wireCount = 0;
+        public static int wire_Count = 0;
         public float[] pinHeight;
 
-        public SpriteRenderer SuccessArea;
+        public GameObject SuccessArea;
+
+        public Text uiText;
 
 
         private void WinState()
         {
-            print("You won!");
+            uiText.GetComponent<Text>().text = "We're in!";
+            print("We're in!");
+            MiniGameManager.instance.GameComplete(true);
             /*
              * Probably will add more things.
              * Lock Background image should be changed as popped open instead of still closed
@@ -29,9 +35,11 @@ namespace LockPick{
         // Start is called before the first frame update
         void Start()
         {
-            print("Does this work?");
             startPosition = transform.position;
-            wireCount++;
+            successPosition = SuccessArea.transform.position;
+            wire_Count++;
+            secondWire.GetComponent<Wire>().SuccessArea = SuccessArea;
+            secondWire.GetComponent<Wire>().uiText = uiText;
 
 
         }
@@ -56,17 +64,40 @@ namespace LockPick{
             if (Input.GetKeyDown(KeyCode.X))
             {
                 //Success if it's in range
-                if(GetComponent<Rigidbody2D>().position.y > pinHeight[wireCount] && GetComponent<Rigidbody2D>().position.y < pinHeight[wireCount] + 0.5f)
+                if(GetComponent<Rigidbody2D>().position.y > pinHeight[wire_Count] && GetComponent<Rigidbody2D>().position.y < pinHeight[wire_Count] + 0.5f)
                 {
-                    print("You succeeded!");
+                    
 
                     //Shift the SuccessArea Sprite to go along with the new Areas to stop the Wire
-
-                    if (wireCount < 3)
+                    if (wire_Count == 1)
                     {
+                        //Replace this with changing a visual UI that shows a text message
+                        //print("Nice! Time to stick the nail in");
+                        uiText.GetComponent<Text>().text = "Nice! Time to Stick the nail in";
+                        successPosition.y += .7f;
+                        SuccessArea.transform.position = successPosition;
+                        
+
+                    }
+                    if (wire_Count == 2)
+                    {
+                        //Replace this with changing a visual UI that shows a text message
+                        //print("Just the paperclip now...");'
+                        uiText.GetComponent<Text>().text = "Just the paperclip now...";
+                        successPosition.y = 1.4f;
+                        SuccessArea.transform.position = successPosition;
+                        
+                    }
+
+
+                    if (wire_Count < 3)
+                    {
+                        //print("You succeeded!");
+
                         Instantiate(secondWire, startPosition + new Vector2(0.5f, 0), Quaternion.identity);
 
                         GetComponent<Rigidbody2D>().gravityScale = 0;
+                        GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
                         this.enabled = false;
                     }
                     else
@@ -74,6 +105,7 @@ namespace LockPick{
                         //Create a "You won!" function
                         WinState();
                         GetComponent<Rigidbody2D>().gravityScale = 0;
+                        GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
                         this.enabled = false;
                         
                     }
@@ -87,7 +119,7 @@ namespace LockPick{
                 }
 
                 //This is to get a position to set parameters for the areas
-                print(GetComponent<Rigidbody2D>().position.y);
+                //print(GetComponent<Rigidbody2D>().position.y);
             }
         }
     }
