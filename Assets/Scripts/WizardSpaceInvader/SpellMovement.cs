52b708393bfe752e25ee9449f3526ca8a234deea
Sam Hilliard <samhilliard51@gmail.com>
2019-11-08 13:50:29 -0800

    Adding my game to the repo
    I haven't been keeping up with makeing commits because I've been having some issues so this is my first commit and its a big one...

---
+++ Assets/Scripts/WizardSpaceInvader/SpellMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellMovement : MonoBehaviour
+{
+    public float speed = 20.0f;
+
+    void Update()
+    {
+        transform.position += Vector3.up * Time.deltaTime * speed;
+    }
+}
---
+++ Assets/Scripts/WizardSpaceInvader/TrollCollisionHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrollCollisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Border" || collision.gameObject.tag == "Player")
+        {
+            print("You lose :(");
+            Destroy(this.gameObject);
+            Time.timeScale = 0;
+        }
+    }
+}
