44bc4afaada1970837929d282a877135e2afaadd
tlieu19 <lieutimothy19@gmail.com>
2019-11-08 18:10:56 -0800

    Added movement of Monster sprite

---
+++ Assets/Scripts/Defend/MonsterActions.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Defend
+{
+
+    public class MonsterActions : MonoBehaviour
+    {
+        private float monsterStopwatch = 3f;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            monsterStopwatch -= Time.deltaTime;
+            MonsterMove();
+        }
+
+        //This is an automated movement of the monster and at the end checks if properly defended
+        void MonsterMove()
+        {
+            print(monsterStopwatch);
+            if(monsterStopwatch < 0)
+            {
+                print("Did you move successfully?");
+                transform.position = new Vector2(transform.position.x, transform.position.y - 1);
+                monsterStopwatch = 3f;
+            }
+            if (transform.position.y <= -2)
+            {
+                DefenseCheck();
+            }
+        }
+        //Checks if there's a defense to stop the monster from killing you
+        void DefenseCheck()
+        {
+            print("Does this work rightn now?");
+        }
+    }
+}
