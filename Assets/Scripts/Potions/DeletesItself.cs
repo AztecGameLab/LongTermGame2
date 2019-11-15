c1a816cf645c0989699085b5aea67b7a768c1b40
Fallium <archer.hovey@gmail.com>
2019-11-14 20:56:46 -0800

    added horizontal/vertical axis controls to potions. particle systems now delete themselves

---
+++ Assets/Scripts/Potions/DeletesItself.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeletesItself : MonoBehaviour
+{
+    float startTime;
+    float timeAlive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+        timeAlive = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > startTime + timeAlive)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
