d3a3fbfad08f36c0da5007719ddf8dcf116fd180
Sam Hilliard <samhilliard51@gmail.com>
2019-12-06 10:48:46 -0800

    Fixed bug

--- Assets/Scripts/WizardSpaceInvader/WizardController.cs
+++ Assets/Scripts/WizardSpaceInvader/WizardController.cs
@@ -23,7 +23,7 @@ public class WizardController : MonoBehaviour
 
     void Start()
     {
-        //AudioManager.instance.PlayMusic(WizardSpaceInvaderClip, 0.6f, 1.0f, true);
+        AudioManager.instance.PlayMusic(WizardSpaceInvaderClip, 0.6f, 1.0f, true);
     }
 
     // Update is called once per frame
